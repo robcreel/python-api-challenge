f87fba68d474feaa3a9f6437fee9dbc9dc77e4b4
Rob <robcreel@gmail.com>
2020-04-19 16:48:25 -0500

    checkpoint

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "493d78637b48da3289d88497944a80f5"
+
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCJOk1icBebAswx5xaNsyuHr0vSU3j7YaQ"
